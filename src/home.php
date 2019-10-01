3cdd21018671bffcc2ac1851f9bc477f5f0059cb
etefft <erich.tefft@gmail.com>
2019-10-01 12:21:14 -0600

    uncommented an error reporting section of code

--- src/home.php
+++ src/home.php
@@ -1,17 +1,12 @@
 <?php 
 $formErr = "";
 
-// if (isset($_GET["empty"])) {
-//     if ($_GET["empty"] === "true") {
-//         $formErr = "You need to fill out all the forms";
-//     }
+if (isset($_GET["empty"])) {
+    if ($_GET["empty"] === "true") {
+        $formErr = "You need to fill out all the forms";
+    }
     
-// } 
+} 
 
-// if (isset($_GET["input"])) {
-//     if ($_GET["input"] == "login") {
-        
-//     }
-// }
 
 ?>
