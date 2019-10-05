36e5b02c090117f9c8280c5fa80df4a3b6e0907d
etefft <erich.tefft@gmail.com>
2019-10-05 11:56:15 -0600

    Removed db code for perry

--- src/db/dbconnect.php
+++ src/db/dbconnect.php
@@ -2,21 +2,3 @@
 
 <!-- This page contains code relating to connecting to the DB -->
 
-<?php 
-
-   //Live connection 
-//$servername = "";
-//$username = "";
-//$password = "";
-
-//Localhost connection
-$servername = "localhost";
-$username = "root";
-$password = "";
-
-//DB name
-$dbname = "biohomie"; 
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-?>
