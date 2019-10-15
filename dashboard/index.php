272f6810922249eb688d0dd22e17b08bae7d3218
etefft <erich.tefft@gmail.com>
2019-10-15 10:30:32 -0600

    updated dashboard to have header and footer

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,18 +1,11 @@
 <?php
 
     require("../src/dashboard.php");
-
+    require("../includes/header.php");
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
     <h3>You are logged in.</h3>
-</body>
-</html>
+<?php
+require("../includes/footer.php");
+
+?>
