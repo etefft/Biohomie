5ab00a4c5c2d37176c511ff542bab23a41353e32
etefft <erich.tefft@gmail.com>
2019-10-15 12:32:11 -0600

    removed placeholder footer text

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
     <!-- This page contains any code that will be on the footer of any page unless a separate footer is needed and created -->
     
     <footer>
-        <h3>test</h3>
+        
     </footer>
     <script src="js/main.js"></script>
     </body>
