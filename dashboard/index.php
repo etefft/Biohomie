47e6cd4fd483f48d49abc886cfc67ba344b3ad2f
etefft <erich.tefft@gmail.com>
2019-10-17 11:03:43 -0600

    updated the nav for dashboard

--- dashboard/index.php
+++ dashboard/index.php
@@ -3,11 +3,7 @@
     require("../src/dashboard.php");
     require("../includes/header.php");
 ?>
-    <?php 
-        $firstnameUser = $_SESSION["firstname"];
-        echo "<h3>Hello, $firstnameUser!</h3>";
-    ?>
-    <p>You are logged in.</p>
+    
 
 <?php
 require("../includes/footer.php");
--- includes/nav-logged.php
+++ includes/nav-logged.php
@@ -1,40 +1,40 @@
    <!-- PAGE INFO -->
 
-    <!-- This page is for any code contained in the navbar -->
+   <!-- This page is for any code contained in the navbar -->
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Biohomie</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+     <a class="navbar-brand" href="#">Biohomie</a>
+     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+       <span class="navbar-toggler-icon"></span>
+     </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <!-- <li class="nav-item active">
+     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+       <ul class="navbar-nav mr-auto">
+         <!-- <li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li> -->
-      <!-- <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li> -->
-      <!-- <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <a class="nav-link" href="#">Link</a>
       </li> -->
-    </ul>
-    <?php echo $_SESSION["firstname"]; ?>
-    <form class="form-inline my-2 my-lg-0" method="post" action="<?php echo htmlspecialchars("../src/verify.php");?>">
-      <input type="hidden" name="logout">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Log Out</button>
-    </form>
-  </div>
-</nav>
+         <li class="nav-item dropdown">
+           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+             aria-haspopup="true" aria-expanded="false">
+             Actions
+           </a>
+           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+             <a class="dropdown-item" href="#">Preferences</a>
+             <a class="dropdown-item" href="#">Creat New Post</a>
+             <div class="dropdown-divider"></div>
+             <form id="logged-nav" class="form-inline my-2 my-lg-0" method="post"
+               action="<?php echo htmlspecialchars("../src/verify.php");?>">
+               <input type="hidden" name="logout">
+               <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Log Out</button>
+             </form>
+           </div>
+         </li>
+       </ul>
+       <?php echo $_SESSION["firstname"]; ?>
+
+     </div>
+   </nav>
